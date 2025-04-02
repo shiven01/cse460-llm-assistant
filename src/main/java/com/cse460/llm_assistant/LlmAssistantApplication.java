ddd418baf7e714631aaab6d86f00a62ddd5f9c25
Shiven Shekar <shivenshekar01@gmail.com>
2025-04-02 10:36:31 -0700

    Initial Commit

---
+++ src/main/java/com/cse460/llm_assistant/LlmAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.cse460.llm_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cse460/llm_assistant/LlmAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse460.llm_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
