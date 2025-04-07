230d0c898f40df96e94a52fc28ffd499bfc7f048
Shiven Shekar <shivenshekar01@gmail.com>
2025-04-07 13:07:14 -0700

    Updated security rules for RDS access

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -12,7 +12,7 @@ resource "aws_db_instance" "main" {
   password                = var.db_password
   db_subnet_group_name    = var.db_subnet_group_name
   vpc_security_group_ids  = [var.db_security_group_id]
-  publicly_accessible     = true  # For development
+  publicly_accessible     = false
   skip_final_snapshot     = true
   backup_retention_period = 7
 
